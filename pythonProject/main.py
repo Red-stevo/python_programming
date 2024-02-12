920c482b1f05f3ecce6e4542623ec1363b48769b
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-02-12 11:52:27 +0300

    pytho sys module

---
+++ pythonProject/main.py
@@ -0,0 +1,12 @@
+import sys
+
+sys.path.append("/home/redsteve/.bashrc")
+
+
+def personal_method():
+    phone_number = {"stephen": "0110555949", "bob": "0110321096"}
+    print(phone_number.values())
+
+
+print(dir(sys))
+# personal_method()
