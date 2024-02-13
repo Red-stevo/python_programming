c1a1b11776bba873b936d1d01014b1acbbdcd0fa
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-02-13 08:33:39 +0300

    finding the number of occurences.Done

--- pythonProject/file_exercise/create_file.py
+++ pythonProject/file_exercise/create_file.py
@@ -5,28 +5,29 @@
 def write_file():
     with open("nums_data.txt", "w") as nums_file:
         for i in range(6):
-            nums_file.write(input("Enter num 1 : ") + "," + input("Enter num 2 : ") + "\n")
+            nums_file.write("\n"+input("Enter num 1 : ") + "," + input("Enter num 2 : "))
 
 
 def read_file():
     # Ask for the number to be search
-    num = input("Enter The Number To Search : ")
+    num = int(input("Enter The Number To Search : "))
     count = 0
-
-
     # open the file for reading.
     with open("nums_data.txt", "r") as nums_file:
-
-        # ensure the number entered is correct.
-        try:
-            num = int(num)
-        except:
-            print("You Entered A Non Integer value.\nTry Again.")
-            return[0,0]
+        file = nums_file.read()
         # search for the number in the file.
-        for line in nums_file.read():
-            tokens = line.split(",")
-            for nums in tokens:
-                if nums == num:
-                    ++count
+        number = file.split(",")
+
+        for i in number:
+            if len(i) > 1:
+                nums = i.split("\n")
+                for x in nums:
+                    x = int(x)
+                    if x == num:
+                        count = count + 1
+            else:
+                i = int(i)
+                if i == num:
+                    count = count + 1
+
     return [num, count]
